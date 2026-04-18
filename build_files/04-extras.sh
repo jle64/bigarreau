ee380486367dd4f82f22444d73048da5261060a8
Jonathan Lestrelin <jonathan@lestrel.in>
2026-04-18 23:07:43 +0200

    ++

--- build_files/04-extras.sh
+++ build_files/04-extras.sh
@@ -3,10 +3,6 @@
 
 set -ouex pipefail
 
-# set correct permissions on polkit rules dir
-chmod 750 /etc/polkit-1
-chgrp -R polkitd /etc/polkit-1
-
 # configure useradd defaults
 sed -i 's|^HOME=.*|HOME=/var/home|' "/etc/default/useradd"
 
