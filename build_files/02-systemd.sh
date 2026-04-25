c351425cb01c67d0ebcf1e3e7f7bdaf3ca9e115d
Jonathan Lestrelin <jonathan@lestrel.in>
2026-04-25 18:28:34 +0200

    ++

---
+++ build_files/02-systemd.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# configure important system services
+
+echo "::group::===========================> Configure system"
+
+set -ouex pipefail
+
+# system
+systemctl enable \
+    apparmor.service \
+    bluetooth.service \
+    cups-browsed.service \
+    cups.socket \
+    firewalld.service \
+    greetd.service \
+    NetworkManager.service \
+    polkit.service \
+    rechunker-group-fix.service \
+    refresh-font-cache.service \
+    tuned-ppd.service \
+    tuned.service \
+    uupd.timer
+
+# system-preset
+systemctl preset \
+    systemd-resolved.service
+
+# user
+systemctl --global enable \
+    flathub-user.service \
+    noctalia-shell.service \
+    opentabletdriver.service
+    
+# user-preset
+systemctl preset --global \
+    flathub-user.service \
+    noctalia-shell.service
+
+# user-wants for Niri
+systemctl add-wants --global niri.service \
+    noctalia-shell.service
