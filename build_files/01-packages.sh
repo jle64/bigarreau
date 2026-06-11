4b66e66df1f0f6629382e50b065ebb2526c1564a
Jonathan Lestrelin <jonathan@lestrel.in>
2026-06-11 11:23:55 +0200

    ++

--- build_files/01-packages.sh
+++ build_files/01-packages.sh
@@ -199,9 +199,6 @@ declare -a packages=(
     gpu-screen-recorder
     kitty
     nautilus
-    waydroid-git
-    waydroid-helper
-    waydroid-script-git
 )
 
 pacman -Sy --noconfirm "${packages[@]}" >/dev/null
